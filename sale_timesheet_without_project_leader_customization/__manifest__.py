dd28e23739914461c8cab2c61a965b4bd129c0c4
Alexander Bayagin <a.bredogen@gmail.com>
2020-07-07 17:39:57 +0300

    Add custom addon for project timesheet cost change

---
+++ sale_timesheet_without_project_leader_customization/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "sale_timesheet_without_project_leader_customization",
+
+    'summary': "sale_timesheet_without_project_leader_customization",
+
+    'description': "",
+
+    'author': "Colibri",
+    'website': "http://colibri.uz",
+
+    'category': 'Sales',
+    'version': '0.1',
+
+    'data': [],
+    
+    "installable": True,
+    # any module necessary for this one to work correctly
+    'depends': ['sale_timesheet'],
+}
---
+++ sale_timesheet_without_project_leader_customization/report/__init__.py
@@ -0,0 +1,1 @@
+from . import project_profitability_report_analysis
